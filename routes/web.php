a67924b14dfa83db54885e5517ff474049dfbc0e
alvinnet21 <alvinnet21@gmail.com>
2025-09-19 09:43:43 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Swagger UI
+// Swagger UI only (docs remain accessible)
 Route::get('/docs', function () {
     return view('swagger');
 });
---
+++ storage/framework/views/b8765846b18a7ed231afe0e0762c86d3.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Swagger UI</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+    <style>
+      html, body { margin: 0; padding: 0; }
+      #swagger-ui { margin: 0; }
+    </style>
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
+    <script>
+      window.onload = () => {
+        window.ui = SwaggerUIBundle({
+          url: '<?php echo e(url('/docs/openapi.yaml')); ?>',
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+          layout: 'StandaloneLayout'
+        });
+      };
+    </script>
+  </body>
+  </html>
+
+<?php /**PATH D:\Tomooo\hahah\tomo_backend\resources\views/swagger.blade.php ENDPATH**/ ?>
